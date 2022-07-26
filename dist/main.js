6d47556e7ff610ea59c10e55049079b3f3aac190
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-07-26 16:26:35 +0800

    build: add webpack for development

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('git gud');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
